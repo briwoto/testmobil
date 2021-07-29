b03f65ff07919d3af13be03fb1d684c04721ebf9
Rahul Singh <rahul.singh@sennder.com>
2021-07-29 17:57:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from appium import webdriver
+import unittest
+
+desired_caps = dict(
+    platformName='iOS',
+    platformVersion='14.5',
+    automationName='xcuitest',
+    deviceName='iPhone Simulator',
+
+)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ session_ios.py
@@ -0,0 +1,20 @@
+from appium import webdriver
+import os
+import time
+
+CWD = os.getcwd()
+HOST = 'http://localhost'
+PORT = '4723'
+APPIUM = 'http://localhost:4723/wd/hub'
+APP_NAME = 'TheApp.app.zip'
+FILEPATH = os.path.join(CWD, APP_NAME)
+CAPS = {
+    'platformName': 'iOS',
+    'platformVersion': '14.5',
+    'deviceName': 'iPhone Simulator',
+    'automationName': 'XCUITest',
+    'app': FILEPATH
+}
+driver = webdriver.Remote(command_executor=APPIUM, desired_capabilities=CAPS)
+driver.quit()
+dummy="123"
